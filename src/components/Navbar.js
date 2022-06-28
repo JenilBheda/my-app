c714a66ed876c30730777baed54e9a53a7c0ab9b
Jenil <jenilbheda@outlook.com>
2022-06-28 21:18:02 +0530

    few change are made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,35 +9,31 @@ export default function Navbar(props) {
     <>
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} navbar-red`}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/TextForm">{props.title}</Link>
+          <Link className="navbar-brand" to={"/"}>{props.title}</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/TextForm`">Home</Link>
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/About">{props.aboutText}</Link>
+                <Link className="nav-link active" to="/About">{props.aboutText}</Link>
               </li>
             </ul>
-            {/* <form className="d-flex mx-1">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form> */}
 
             <div className="d-flex">
-              <div className="bg-primary rounded mx-2" onClick={()=>{props.toggleMode('primary')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div>
-              <div className="bg-danger rounded mx-2" onClick={()=>{props.toggleMode('danger')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div>
-              <div className="bg-success rounded mx-2" onClick={()=>{props.toggleMode('success')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div>
-              <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode('warning')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div>
-              <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode('light')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div>
-              <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode('dark')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div>
+              {/* <div className="bg-primary rounded mx-2 my-1" onClick={()=>{props.toggleMode('primary')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div>
+              <div className="bg-danger rounded mx-2 my-1" onClick={()=>{props.toggleMode('danger')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div>
+              <div className="bg-success rounded mx-2 my-1" onClick={()=>{props.toggleMode('success')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div>
+              <div className="bg-warning rounded mx-2 my-1" onClick={()=>{props.toggleMode('warning')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div>
+              <div className="bg-warning rounded mx-2 my-1" onClick={()=>{props.toggleMode('light')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div> */}
+              {/* <div className="bg-warning rounded mx-2 my-1" onClick={()=>{props.toggleMode('dark')}} style={{height:'40px', width:'30px', cursor:'pointer'}}></div> */}
             </div>
-            <div className={`form-check form-switch mx-1 text-${props.mode==='light'?'dark':'light'}`}>
+            <div className={`form-check form-switch mx-1 my-1 text-${props.mode==='dark'?'light':'dark'}`}>
               <input className="form-check-input" onClick={()=>{props.toggleMode(null)}} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Toggle Mode</label>
+              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
             </div>
           </div>
         </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,14 +6,14 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=> {
-        console.log("Uppercase was clicked" + text);
+        // console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText);
         props.showAlert("Coverted to uppercase!", "success")
     }
     
     const handleLoClick = ()=> {
-        console.log("Lowercase was clicked" + text);
+        // console.log("Lowercase was clicked" + text);
         let newText = text.toLowerCase();
         setText(newText);
         props.showAlert("Coverted to lowercase!", "success")
@@ -29,11 +29,16 @@ export default function TextForm(props) {
         props.showAlert("Coverted to pascalcase !", "success")
     }
     
+    const handleClearClick = () => {
+        // console.log("I am cleared")
+        let newText = '';
+        setText(newText);
+        props.showAlert("Cleared !", "success")
+    }
+    
     const handleCopy = () => {
-        console.log("I am copy")
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        // console.log("I am copy")
+        navigator.clipboard.writeText(text);
         props.showAlert("Copied!", "success")
     }
     
@@ -45,35 +50,36 @@ export default function TextForm(props) {
 
     
     const handleOnChange = (event)=> {
-        console.log("On change");
+        // console.log("On change");
         setText(event.target.value);
     }
 
-    const [text, setText] = useState('Enter Text Here');
+    const [text, setText] = useState('');
     // text = "new text"; // Wrong way to change the state
     // setText = "new text"; // Correct way to change the state
     return (
         <>
         <div className='container' style={{color: props.mode==='light'?'#042743':'white'}}>
 
-            <h1>{props.heading} </h1>
+            <h1 className='mb-4'>{props.heading} </h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'white':'grey', color: props.mode==='light'?'#042743':'white'}} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'white':'#042743', color: props.mode==='light'?'#042743':'white'}} id="myBox" rows="8"></textarea>
             </div>
 
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-1" onClick={pascalCase}>Convert to PascalCase</button>
-            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={pascalCase}>Convert to PascalCase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
 
         <div className="conatiner my-3" style={{color: props.mode==='light'?'#042743':'white'}}>
             <h1>Your text summary</h1>
-            <p>{text.length>0 ? text.trim().split(" ").length:0} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <p>{text.length>0 ? text.trim().split(/\s+/).length:0} words and {text.length} characters</p>
+            <p>{0.008 * text.split(/\s+/).filter((element)=> {return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text.length>0?text:"Enter Something in the textbox to preview it here"}</p>
+            <p>{text.length>0?text:"Nothing to preview"}</p>
         </div>
         </>
 
