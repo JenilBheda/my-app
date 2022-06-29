d648245071130cc0f925a75cfd629b724b093611
Jenil <jenilbheda@outlook.com>
2022-06-29 15:52:39 +0530

    Textutils

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [mode, setMode] = useState('light'); // Whether the dark mode is enable or not
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
@@ -21,7 +21,7 @@ function App() {
     }, 3000);
   }
 
-  const removeBodyClasses = ()=>{
+  const removeBodyClasses = () => {
     document.body.classList.remove('bg-primary');
     document.body.classList.remove('bg-dark');
     document.body.classList.remove('bg-light');
@@ -30,11 +30,11 @@ function App() {
     document.body.classList.remove('bg-success');
   }
 
-  const toggleMode = (cls)=>{
+  const toggleMode = (cls) => {
     removeBodyClasses();
     console.log(cls)
-    document.body.classList.add('bg-'+cls)
-    if(mode === 'light'){
+    document.body.classList.add('bg-' + cls)
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
@@ -42,13 +42,13 @@ function App() {
       // setInterval(() => { // to set the notification on title to attract people BUT NOT GOOD 
       //   document.title = 'MyApp is Amazing'
       // }, 2000);
-      
+
       // setInterval(() => { // to set the notification on title to attract people BUT NOT GOOD 
       //   document.title = 'Install MyApp Now!!!'
       // }, 1500);
     }
-    
-    else{
+
+    else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Dark mode has been disbaled", "success");
@@ -58,25 +58,25 @@ function App() {
 
   return (
     <>
-    <Router> 
+      <Router>
         <Navbar
-            title="TextUtils" 
-            aboutText="About TextUtils" 
-            homeText="Home" 
-            mode={mode} 
-            toggleMode={toggleMode} 
-          /> 
-         <Alert alert={alert} /> 
+          title="TextUtils"
+          aboutText="About TextUtils"
+          homeText="Home"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
 
-         <div className="container"> 
-          <Routes> 
-             <Route exact path="/" element={<TextForm showAlert={showAlert} 
-                   heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces" 
-                    mode={mode} 
-                    toggleMode={toggleMode}/>}/> 
-             <Route exact path="/About"  
-                    element={<About mode={mode} toggleMode={toggleMode}/>}  /> 
-             {/* <Route 
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<TextForm showAlert={showAlert}
+              heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces"
+              mode={mode}
+              toggleMode={toggleMode} />} />
+            <Route exact path="/About"
+              element={<About mode={mode} toggleMode={toggleMode} />} />
+            {/* <Route 
                 exact path="/" 
                 index 
                 element={ 
@@ -88,23 +88,23 @@ function App() {
                  />
                }
              /> */}
-           </Routes>
-         </div>
-       </Router>
+          </Routes>
+        </div>
+      </Router>
 
 
       {/* <Navbar title = "MyApp" aboutText = "About MyApp" /> */}
-       {/* <Navbar/> */}
-       {/* <Navbar title = "MyApp" mode={mode} toggleMode={toggleMode} />  */}
-       {/* <Alert alert={alert} /> */}
-       {/* <About/> */}
+      {/* <Navbar/> */}
+      {/* <Navbar title = "MyApp" mode={mode} toggleMode={toggleMode} />  */}
+      {/* <Alert alert={alert} /> */}
+      {/* <About/> */}
       {/* <div className="container my-3"> */}
-        
+
       {/* <TextForm showAlert={showAlert} heading="Word Counter, Character Counter, Remove extra spaces" mode={mode}/> */}
       {/* <About aboutText = "About MyApp" mode={mode}/> */}
       {/* </div> */}
 
-      
+
     </>
   );
 }
